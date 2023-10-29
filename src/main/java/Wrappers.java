b225c9c4c222ee2c279c57fb52719ffd95ccca51
BabunovaIrina <143878530+BabunovaIrina@users.noreply.github.com>
2023-10-29 22:24:18 +0300

    Update Wrappers.java

--- src/main/java/Wrappers.java
+++ src/main/java/Wrappers.java
@@ -2,11 +2,10 @@ public class Wrappers {
     // auto boxing - автоматическое обертывание примитива в класс обретку над ним. Можно делать так Integer i = Integer.valueOf(152500);
     Byte b = 5;
     Short sh = 25545;
-    static Integer i = 15250000;
+    Integer i = 15250000;
     Long l = 254000000000L;
     Float f = 2.6F;
     Double d = 2.33;
     Character ch = 'N';
     Boolean fl = false;
-
 }
